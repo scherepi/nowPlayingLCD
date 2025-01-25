5d0677c0688682c918b90ff3568905d108631e8b
j. schere <scherejoaquin@gmail.com>
2025-01-25 14:43:50 -0600

    access token grabbed

--- getNowPlaying.py
+++ getNowPlaying.py
@@ -1,2 +1,18 @@
 import requests
 import oauthlib
+
+clientID = open("clientId.txt").readline()[:-1] # gotta clip the newline, once again
+clientSecret = open("clientSecret.txt").readline()[:-1] # again...
+
+data = {
+	'grant_type': 'client_credentials',
+	'client_id': clientID,
+	'client_secret': clientSecret
+}
+
+accessToken = requests.post("https://accounts.spotify.com/api/token", data=data).json()["access_token"]
+
+headers = {
+
+}
+
